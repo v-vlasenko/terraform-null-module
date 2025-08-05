2414a08b21a61d6d57fd73ad5cef9b97d3ff44de
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2025-08-05 16:12:57 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,12 @@ resource "null_resource" "resource" {
   }
 }
 
+resource "null_resource" "resource2" {
+  triggers = {
+    number = "${var.trigger}"
+  }
+}
+
 output "null_resource_id" {
   description = "The `id` of the `null_resource` resource in this module."
   value       = "${null_resource.resource.id}"
