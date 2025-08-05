9345b00db98fae110b96bd10311e7ba08f27af8c
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2025-08-05 17:44:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,11 +22,6 @@ variable "trigger" {
   default     = "one"
 }
 
-resource "null_resource" "resource" {
-  triggers = {
-    number = "${var.trigger}"
-  }
-}
 
 output "null_resource_id" {
   description = "The `id` of the `null_resource` resource in this module."
