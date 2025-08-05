b2c37d7256ad823a05c13167cacac47a44bca088
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2025-08-05 17:44:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,19 +22,6 @@ variable "trigger" {
   default     = "one"
 }
 
-resource "null_resource" "resource" {
-  triggers = {
-    number = "${var.trigger}"
-  }
-}
-
-resource "random_pet" "test_pet_with_name" {
-  count = 12
-  keepers = {
-    vlad = "Keeperz"
-  }
-}
-
 output "null_resource_id" {
   description = "The `id` of the `null_resource` resource in this module."
   value       = "${null_resource.resource.id}"
